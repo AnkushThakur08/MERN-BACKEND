b76aa9d3e39057787389da8c124e086c3875a5db
Ankush Thakur <atpara99@gmail.com>
2022-01-19 13:16:10 +0530

    productUpdation Completed

--- controllers/product.js
+++ controllers/product.js
@@ -101,5 +101,41 @@ exports.deleteProduct = (req, res) => {
 
 // Update Controller
 exports.updateProduct = (req, res) => {
-  
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+
+  form.parse(req, (err, fields, file) => {
+    if (err) {
+      return res.status(400).json({
+        err: "Problem with Image or FILE",
+      });
+    }
+
+    // UPDATION CODE
+    let product = req.product;
+    product = _.extend(product, fields);
+    // extend method, takes the existing  value and Update it take the value from the product (i.e DB) & update it with the fields
+
+    if (file.photo) {
+      if (file.photo.size > 4000000) {
+        return res.status(400).json({
+          err: "Image Limit is 3 MB",
+        });
+      }
+
+      product.photo.data = fs.readFileSync(file.photo.filepath);
+      product.photo.contentType = file.photo.mimetype;
+    }
+
+    // Save to the DB
+    product.save((err, product) => {
+      if (err) {
+        res.status(400).json({
+          error: "Updation of Product Failed",
+        });
+      }
+
+      res.json(product);
+    });
+  });
 };
