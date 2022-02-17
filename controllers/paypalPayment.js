3e64bc3e643b3e703cea373bca71242f38b3520c
Ankush Thakur <atpara99@gmail.com>
2022-02-17 15:53:51 +0530

    Bug Fixed

--- controllers/paypalPayment.js
+++ controllers/paypalPayment.js
@@ -2,9 +2,9 @@ const braintree = require("braintree");
 
 const gateway = new braintree.BraintreeGateway({
   environment: braintree.Environment.Sandbox,
-  merchantId: "useYourMerchantId",
-  publicKey: "useYourPublicKey",
-  privateKey: "useYourPrivateKey",
+  merchantId: "vft2jmbb6dzcqzr6",
+  publicKey: "g37wz6555rbrqc9x",
+  privateKey: "6ed3c6cb0d498c3437143e7bdfb96b80",
 });
 
 exports.getToken = (req, res) => {
--- routes/paypalPayment.js
+++ routes/paypalPayment.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 const { isSignedIn, isAuthenticated } = require("../controllers/auth");
 const { processPayment, getToken } = require("../controllers/paypalPayment");
+const { getUserById } = require("../controllers/user");
+
+router.param("userId", getUserById);
 
 router.get("/payment/gettoken/:userId", isSignedIn, isAuthenticated, getToken);
 
